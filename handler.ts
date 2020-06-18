821f52eda42074e7eeba3cd43c332c83445d284d
Grzegorz Musiał <grzegorz@musial.tk>
2020-06-19 01:10:35 +0200

    feat: somewhat dynamic responses with cities

--- handler.ts
+++ handler.ts
@@ -1,11 +1,18 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 
+let counter = 0;
+const cities = [
+    "Conventry",
+    "Wrocław",
+    "Frankfurt"
+]
 export const handle = async (event: APIGatewayProxyEvent) => {
   console.log(event.body);
+  counter = (counter + 1) % cities.length;
   return {
     statusCode: 200,
     body: JSON.stringify({
-      fulfillmentText: "response!",
+      fulfillmentText: cities[counter],
     }),
   };
 };
